eb5398708191191761f2a3dfab78989c4c6702b3
Mikymac <mikymacmc@hotmail.co.uk>
2021-03-16 14:10:31 +0000

    message sending test

--- bot.py
+++ bot.py
@@ -49,8 +49,15 @@ async def idcall(ctx, *, test: discord.TextChannel):
 	await ctx.send(f"Here's your mentioned channel ID: {chanID}")
 
 @bot.command()
-async def send(ctx, *, channel: discord.TextChannel, message):
-	await channel.send(message)
+async def send(ctx, *args):
+	channel = discord.utils.get(ctx.guild.channels, name=arg[0])
+	del args[0]
+	await channel.send('{} arguments: {}'.format(len(args), ', '.join(args)))
+
+
+
+
+
 
 
 
