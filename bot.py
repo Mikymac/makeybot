3107b1ba2ecf5d5e6b5965567d4e69c65aff121d
Mikymac <mikymacmc@hotmail.co.uk>
2021-04-06 15:54:53 +0100

    Re-factoring code.

--- bot.py
+++ bot.py
@@ -56,7 +56,7 @@ async def restart(ctx):
 
 @bot.command()
 async def poweroff(ctx):
-	if(admin in ctx.author.roles or data["debugID"] in ctx.author.id):
+	if(admin in ctx.author.roles or data["debugID"] == ctx.author.id):
 		await audit(f'{ctx.author.display_name} has Powered me Down. I will need to be powered on again manually.')
 		await bot.logout()
 
