7b6a3a580c7c93d062e855f97c0ff98b85239bd0
Mikymac <mikymacmc@hotmail.co.uk>
2021-04-07 00:12:34 +0100

    Beginning setting up the set code a readcode functions

--- bot.py
+++ bot.py
@@ -15,9 +15,9 @@ with open("config.json", "r+") as conf:
 		with open("toAppend.txt", "r+") as appen:
 			content = appen.readlines()
 			newdict = {content[0].strip('\n'): content[1].strip('\n')}
-			appen.truncate(0)
-			data.update(newdict)
 			os.remove("toAppend.txt")
+			data.update(newdict)
+			conf.seek(0)
 			json.dump(data, conf, ensure_ascii=False, indent=4)
 			print("Should've done soemthing")
 	except:
