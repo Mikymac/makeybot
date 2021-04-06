ba01251e871009e41f3874ba80026eb369da5618
Mikymac <mikymacmc@hotmail.co.uk>
2021-04-06 22:39:09 +0100

    Beginning setting up the set code a readcode functions

--- bot.py
+++ bot.py
@@ -68,7 +68,8 @@ async def setcode(ctx, code):
 	print(code)
 	async with aiofiles.open("code.txt", "a+") as test:
 		test.seek(0)
-		await test.write(code + "\n")
+		test.flush()
+		await test.write(code)
 		print(await test.read())
 
 @bot.command()
